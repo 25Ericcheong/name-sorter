e47b7ff4301c078845dc559fc06cec12dbb3ac48
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-21 13:11:48 +1000

    completed sorting name algorithm and file creation

--- src/main/java/NameSorter.java
+++ src/main/java/NameSorter.java
@@ -1,60 +1,33 @@
 package main.java;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class NameSorter {
     public static void main(String[] args) throws IOException {
         String filepath = args[0];
+        String WRITE_FILE_PATH = "main/resources/names-sorted.txt";
+        FileRead fileRead = new FileRead (filepath);
+        FileWrite fileWrite = new FileWrite(WRITE_FILE_PATH);
+
         List<Person> names = new ArrayList<>();
 
         try {
-            names = readFile(filepath);
+            names = fileRead.getAllNames();
         } catch (IOException ex) {
             ex.printStackTrace();
             System.out.println("File does not exist");
         }
+
         Collections.sort(names);
 
         try {
-            writeFile(names);
+            fileWrite.createFile(names);
         } catch (IOException ex) {
             ex.printStackTrace();
-            System.out.println("Error occurred while writing file");
+            System.out.println("Error occurred while creating file");
         }
-    }
-
-    private static List<Person> readFile(String filepath) throws IOException {
-        List<Person> names = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new FileReader(filepath));
-
-        String name, lastName, givenNames;
-        ArrayList<String> splitName;
-        while ((name = reader.readLine()) != null) {
-            splitName = new ArrayList<>(Arrays.asList(name.split(", ")));
-            lastName = splitName.get(0);
-            givenNames = splitName.get(1);
-
-            names.add(new Person(givenNames, lastName));
-        }
-
-        reader.close();
-        return names;
-    }
-
-    private static void writeFile(List<Person> names) throws IOException {
-        FileWriter writer = new FileWriter("main/resources/names-sorted.txt", false);
-        for (Person name : names) {
-            System.out.println(name);
-            writer.write(name + System.lineSeparator());
-        }
-        System.out.println("Finished: created names-sorted.txt");
-        writer.close();
     }
 }
--- src/test/FileWriteTest.java
+++ src/test/FileWriteTest.java
@@ -1,4 +1,36 @@
 package test;
 
+import main.java.FileRead;
+import main.java.FileWrite;
+import main.java.Person;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class FileWriteTest {
+    File createdFile;
+
+    @Test
+    public void shouldCreateFile() throws IOException {
+        FileRead fileRead = new FileRead("src/test/resources/unsorted-name.txt");
+        List<Person> people = fileRead.getAllNames();
+
+        FileWrite fileWrite = new FileWrite("src/test/resources/test-sorted-name.txt");
+        fileWrite.setFilePath("src/test/resources/sorted-name.txt");
+        fileWrite.createFile(people);
+
+        createdFile = new File("src/test/resources/sorted-name.txt");
+        Assert.assertTrue("File should be created", createdFile.exists());
+    }
+
+    @After
+    public void clean(){
+        if (createdFile.exists()) {
+            createdFile.delete();
+        }
+    }
 }
