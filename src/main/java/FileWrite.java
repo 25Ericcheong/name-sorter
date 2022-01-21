ca02bb1b56261bf18f30a50a68bee48992bdfb61
25Ericcheong <ericcheongkaikit@gmail.com>
2022-01-21 13:34:10 +1000

    finished rest of code doccumentation

--- src/main/java/FileWrite.java
+++ src/main/java/FileWrite.java
@@ -4,17 +4,33 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Used to create file based on provided file path
+ */
 public class FileWrite {
     String filepath;
 
+    /**
+     * Filepath is by default already provided in program but can be changed if needed
+     * @param filePath location used to specific creation of file
+     */
     public FileWrite(String filePath) {
         this.filepath = filePath;
     }
 
+    /**
+     * To change file path for file creation if needed
+     * @param filePath that will replace old filepath specified during creation of class
+     */
     public void setFilePath(String filePath) {
         this.filepath = filePath;
     }
 
+    /**
+     * Creates a file with list of names given as arguments with each name having a separate line in file
+     * @param names of person acquired from the file read previously
+     * @throws IOException if an error is to occur during the creation of the file
+     */
     public void createFile(List<Person> names) throws IOException {
         FileWriter writer = new FileWriter(filepath, false);
         for (Person name : names) {
